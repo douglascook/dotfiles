ffa04cd3ac9827b0173d196f52603a95cbed8286
Douglas Cook <douglas@kheironmed.com>
2024-02-26 16:51:11 +0000

    Update git diff algorithm and renaming detection

--- configure-git.sh
+++ configure-git.sh
@@ -4,3 +4,10 @@ git config --global pull.rebase false
 # Better way to display merge conflicts. 
 # See vimwiki https://github.com/douglascook/vimwiki/blob/master/dev/git.md#merge-conflict-diffs
 git config --global merge.conflictstyle zdiff3
+
+# Improved diff algorithm.
+# See https://luppeng.wordpress.com/2020/10/10/when-to-use-each-of-the-git-diff-algorithms/
+git config --global diff.algorithm histogram
+
+# Better detection of renamed files.
+git config --global diff.renames copies
