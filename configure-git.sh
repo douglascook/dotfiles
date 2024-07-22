37bd44c433f294e5a26d41352ebed66de46db1fe
Douglas Cook <douglas@kheironmed.com>
2024-07-22 12:53:51 +0100

    Default to autosquash in git config

--- configure-git.sh
+++ configure-git.sh
@@ -1,7 +1,7 @@
 # Merge when pulling from remote if it has diverged from local
 git config --global pull.rebase false
 
-# Better way to display merge conflicts. 
+# Better way to display merge conflicts.
 # See vimwiki https://github.com/douglascook/vimwiki/blob/master/dev/git.md#merge-conflict-diffs
 git config --global merge.conflictstyle zdiff3
 
@@ -11,3 +11,7 @@ git config --global diff.algorithm histogram
 
 # Better detection of renamed files.
 git config --global diff.renames copies
+
+# Enable autosquash by default when rebasing, for working with fixup commits.
+# See https://andrewlock.net/smoother-rebases-with-auto-squashing-git-commits/
+git config --global rebase.autosquash true
